698dffd1983ee4b5f64c2d1f4cdd6e22c78d6618
Wincioor11 <wincioor11@gmail.com>
2019-11-05 17:25:29 +0100

    04.11.2019 - Michał Winciorek - fix connection - change mainpadsteering:   execution

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PC
-1.11.19 niemiaszek - PadSteering class and initial version of PC-Jetson-Odroid scripts
 04.11.2019 - Michał Winciorek
 - fix connection 
 - change mainpadsteering:
   execution
+  
+1.11.19 niemiaszek - PadSteering class and initial version of PC-Jetson-Odroid scripts
--- mainpadsteering.py
+++ mainpadsteering.py
@@ -1,8 +1,12 @@
-from .connectionlaptop import Connection
+from connectionlaptop import Connection
 
-IP_ADDRESS = ' 192.168.137.68'
+IP_ADDRESS = '192.168.137.208'
+PORT = 8181
 
-conn_thread = Connection(IP_ADDRESS, 8080)
-conn_thread.start()
+if __name__ == "__main__":
+    conn_flag = True  # flaga -> opuszczanie pętli od razu po połączeniu
+    while conn_flag :
+        conn_thread = Connection(IP_ADDRESS, PORT)
+        conn_flag = not conn_flag
 
-
+    conn_thread.start()
